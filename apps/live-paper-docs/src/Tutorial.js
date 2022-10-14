8b8af0516057c465e18090e06fa816f10f628cea
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2022-10-14 14:49:26 +0200

    add Maya bit to tutorial video

--- apps/live-paper-docs/src/Tutorial.js
+++ apps/live-paper-docs/src/Tutorial.js
@@ -51,7 +51,7 @@ export default class Tutorial extends React.Component {
           <div className="block-main-header">Demonstration of Live Papers</div>
           <div className="block-text">
             <div class="video-container">
-              <YouTube videoId='tnAcMYpKiVk' opts={opts} onReady={this.onReady} />
+              <YouTube videoId='h6BxdWN1Jj4' opts={opts} onReady={this.onReady} />
             </div>
             <br />
             <div style={{
@@ -69,97 +69,102 @@ export default class Tutorial extends React.Component {
             <br />
             <div style={{ lineHeight: 1.75 }}>
               <ul>
-                <li><a href onClick={() => this.changeTime(0)}>Introduction to Live Papers</a></li>
                 <li>
-                  <a href onClick={() => this.changeTime(112)}>Live Paper Platform</a>
+                  <a href onClick={() => this.changeTime(0)}>Introduction to tutorial</a>
                   <ul>
-                    <li><a href onClick={() => this.changeTime(156)}>Walkthrough a sample Live Paper</a></li>
+                    <li><a href onClick={() => this.changeTime(37)}>The problem... and a solution</a></li>
                   </ul>
                 </li>
                 <li>
-                  <a href onClick={() => this.changeTime(490)}>Live Paper Builder</a>
+                  <a href onClick={() => this.changeTime(121+112)}>Live Paper Platform</a>
+                  <ul>
+                    <li><a href onClick={() => this.changeTime(121+156)}>Walkthrough a sample Live Paper</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href onClick={() => this.changeTime(121+490)}>Live Paper Builder</a>
                   <li>
-                    <a href onClick={() => this.changeTime(505)}>EBRAINS account and the Collaboratory</a>
+                    <a href onClick={() => this.changeTime(121+505)}>EBRAINS account and the Collaboratory</a>
                     <ul>
-                      <li><a href onClick={() => this.changeTime(512)}>Request EBRAINS account</a></li>
-                      <li><a href onClick={() => this.changeTime(561)}>Intro to Collaboratory & Collabs</a></li>
-                      <li><a href onClick={() => this.changeTime(617)}>Create a new Collab</a></li>
-                      <li><a href onClick={() => this.changeTime(712)}>Overview of Teams & Live Paper permissions</a></li>
+                      <li><a href onClick={() => this.changeTime(121+512)}>Request EBRAINS account</a></li>
+                      <li><a href onClick={() => this.changeTime(121+561)}>Intro to Collaboratory & Collabs</a></li>
+                      <li><a href onClick={() => this.changeTime(121+617)}>Create a new Collab</a></li>
+                      <li><a href onClick={() => this.changeTime(121+712)}>Overview of Teams & Live Paper permissions</a></li>
                     </ul>
                   </li>
                   <ul>
                     <li>
-                      <a href onClick={() => this.changeTime(821)}>Start creating a new live paper</a>
+                      <a href onClick={() => this.changeTime(121+821)}>Start creating a new live paper</a>
                       <ul>
-                        <li><a href onClick={() => this.changeTime(868)}>Loading metadata via DOI</a></li>
-                        <li><a href onClick={() => this.changeTime(890)}>Loading metadata via PDF</a></li>
-                        <li><a href onClick={() => this.changeTime(975)}>Entering/updating metadata manually</a></li>
+                        <li><a href onClick={() => this.changeTime(121+868)}>Loading metadata via DOI</a></li>
+                        <li><a href onClick={() => this.changeTime(121+890)}>Loading metadata via PDF</a></li>
+                        <li><a href onClick={() => this.changeTime(121+975)}>Entering/updating metadata manually</a></li>
                       </ul>
                     </li>
-                    <li><a href onClick={() => this.changeTime(1177)}>Preview live papers</a></li>
+                    <li><a href onClick={() => this.changeTime(121+1177)}>Preview live papers</a></li>
                     <li>
-                      <a href onClick={() => this.changeTime(1200 )}>Adding resource sections</a>
+                      <a href onClick={() => this.changeTime(121+1200 )}>Adding resource sections</a>
                       <ul>
-                        <li><a href onClick={() => this.changeTime(1235)}>Generic section</a></li>
+                        <li><a href onClick={() => this.changeTime(121+1235)}>Generic section</a></li>
                         <li>
-                          <a href onClick={() => this.changeTime(1250)}>Common features</a>
+                          <a href onClick={() => this.changeTime(121+1250)}>Common features</a>
                           <ul>
-                            <li><a href onClick={() => this.changeTime(1285)}>Adding entries to section</a></li>
-                            <li><a href onClick={() => this.changeTime(1316)}>Expanding input area</a></li>
-                            <li><a href onClick={() => this.changeTime(1351)}>Re-ordering entries in section</a></li>
-                            <li><a href onClick={() => this.changeTime(1362)}>Grouping items into tabs</a></li>
-                            <li><a href onClick={() => this.changeTime(1430)}>Editing source code</a></li>
-                            <li><a href onClick={() => this.changeTime(1517)}>Deleting items</a></li>
-                            <li><a href onClick={() => this.changeTime(1524)}>Simple multi-input tool</a></li>
-                            <li><a href onClick={() => this.changeTime(1638)}>Collapsing a live paper section</a></li>
-                            <li><a href onClick={() => this.changeTime(1657)}>Re-ordering live paper sections</a></li>
-                            <li><a href onClick={() => this.changeTime(1672)}>Deleting a live paper section</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1285)}>Adding entries to section</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1316)}>Expanding input area</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1351)}>Re-ordering entries in section</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1362)}>Grouping items into tabs</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1430)}>Editing source code</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1517)}>Deleting items</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1524)}>Simple multi-input tool</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1638)}>Collapsing a live paper section</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1657)}>Re-ordering live paper sections</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1672)}>Deleting a live paper section</a></li>
                           </ul>
                         </li>
                         <li>
-                          <a href onClick={() => this.changeTime(1701)}>Morphology section</a>
+                          <a href onClick={() => this.changeTime(121+1701)}>Morphology section</a>
                           <ul>
-                            <li><a href onClick={() => this.changeTime(1775)}>Add from external database</a></li>
+                            <li><a href onClick={() => this.changeTime(121+1775)}>Add from external database</a></li>
                           </ul>
                         </li>
                         <li>
-                          <a href onClick={() => this.changeTime(1949)}>Electrophysiology section</a>
+                          <a href onClick={() => this.changeTime(121+1949)}>Electrophysiology section</a>
                           <ul>
-                            <li><a href onClick={() => this.changeTime(2047)}>Add from external database</a></li>
+                            <li><a href onClick={() => this.changeTime(121+2047)}>Add from external database</a></li>
                           </ul>
                         </li>
                         <li>
-                          <a href onClick={() => this.changeTime(2081)}>Models section</a>
+                          <a href onClick={() => this.changeTime(121+2081)}>Models section</a>
                           <ul>
-                            <li><a href onClick={() => this.changeTime(2114)}>Add from external database</a></li>
+                            <li><a href onClick={() => this.changeTime(121+2114)}>Add from external database</a></li>
                           </ul>
                         </li>
                         <li>
-                          <a href onClick={() => this.changeTime(2182)}>Custom HTML/Markdown section</a>
+                          <a href onClick={() => this.changeTime(121+2182)}>Custom HTML/Markdown section</a>
                           <ul>
-                            <li><a href onClick={() => this.changeTime(2252)}>Example</a></li>
+                            <li><a href onClick={() => this.changeTime(121+2252)}>Example</a></li>
                           </ul>
                         </li>
                       </ul>
                     </li>
-                    <li><a href onClick={() => this.changeTime(2358)}>Download live papers</a></li>
-                    <li><a href onClick={() => this.changeTime(2433)}>Save live papers</a></li>
+                    <li><a href onClick={() => this.changeTime(121+2358)}>Download live papers</a></li>
+                    <li><a href onClick={() => this.changeTime(121+2433)}>Save live papers</a></li>
                     <li>
-                      <a href onClick={() => this.changeTime(2503)}>Submit live papers</a>
+                      <a href onClick={() => this.changeTime(121+2503)}>Submit live papers</a>
                       <ul>
-                        <li><a href onClick={() => this.changeTime(2517)}>Publish live paper</a></li>
+                        <li><a href onClick={() => this.changeTime(121+2517)}>Publish live paper</a></li>
                         <li>
-                          <a href onClick={() => this.changeTime(2559)}>Password-protect live paper</a>
+                          <a href onClick={() => this.changeTime(121+2559)}>Password-protect live paper</a>
                           <ul>
-                            <li><a href onClick={() => this.changeTime(2579)}>Example</a></li>
+                            <li><a href onClick={() => this.changeTime(121+2579)}>Example</a></li>
                           </ul>
                         </li>
                       </ul>
                     </li>
-                    <li><a href onClick={() => this.changeTime(2640)}>Resume working on an existing live paper</a>
+                    <li><a href onClick={() => this.changeTime(121+2640)}>Resume working on an existing live paper</a>
                       <ul>
-                        <li><a href onClick={() => this.changeTime(2658)}>Load existing live paper project from file</a></li>
-                        <li><a href onClick={() => this.changeTime(2702)}>Load existing live paper project from KG</a></li>
+                        <li><a href onClick={() => this.changeTime(121+2658)}>Load existing live paper project from file</a></li>
+                        <li><a href onClick={() => this.changeTime(121+2702)}>Load existing live paper project from KG</a></li>
                       </ul>
                     </li>
                   </ul>
