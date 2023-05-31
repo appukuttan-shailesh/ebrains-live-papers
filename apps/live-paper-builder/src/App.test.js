45bda7c1599b9838da29a5c044047e5077d76338
gara-elhaddad <107392013+gara-elhaddad@users.noreply.github.com>
2023-05-31 15:36:39 +0200

    Update App.test.js

--- apps/live-paper-builder/src/App.test.js
+++ apps/live-paper-builder/src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor ,  wait, fireEvent  } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from "react";
+import { SnackbarProvider } from "notistack";
+import { createMemoryHistory } from "history";
+//import '@testing-library/jest-dom/extend-expect';
+import userEvent  from "@testing-library/user-event";
 import App from "./App";
+import {BrowserRouter, MemoryRouter, Router} from 'react-router-dom';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
+import CreateLivePaperLoadPDFData from './CreateLivePaperLoadPDFData';
+//import { SnackbarProvider, enqueueSnackbar } from 'notistack'
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+//const user = userEvent.setup({delay:null});
+const Button = ({onClick, children}) => (
+  <button onClick={onClick}>{children}</button>
+)
+
+test('renders learn react link', async () => {
+  const mockCallBack = jest.fn();
+  const user = userEvent.setup();
+  jest.setTimeout(30000); 
+  const userauth= [ {token:[], context:[]}] ;
+  const history = createMemoryHistory(); // or createBrowserHistory
+  const {rerender} =render(
+        
+         <CreateLivePaper />
+          
+   
+    );
+  //const user = userEvent.setup();
+  const linkElement = screen.getByText(/Skip/i);
+  await expect (linkElement).toBeInTheDocument();
+  
+  //user.click(  await screen.getByRole("button", { name: /Create New/i }));
+  user.click(  await screen.getByRole("button", { name: /Skip/i }));
+  //user.click(  await screen.getByRole("button", { name: /ReferenceError: debug is not defined/i }));
+  //await fireEvent.click(  await screen.getByText(/Open Documentation/i));
+ 
+  //screen.debug();
+  await waitFor(async () => {   
+    
+    expect(  await screen.getByText(/Where to find underlying data/i)).toBeInTheDocument();
+  }
+  , 100000
+  
+  );
+  
+  
+ 
+
+ });
