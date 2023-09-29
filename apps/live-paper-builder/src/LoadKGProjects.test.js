676a9d4e8bd9910952318225e69937915d529492
gara-elhaddad <107392013+gara-elhaddad@users.noreply.github.com>
2023-09-29 10:05:04 +0200

    Create LoadKGProjects.test.js

---
+++ apps/live-paper-builder/src/LoadKGProjects.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import LoadKGProjects from "./CreateLivepaper/CreateNewLivePaper/LoadKGProjects";
+// Mock any props that are required for the component
+const defaultProps = {
+  kg_project_list: [],
+  open: false,
+  onClose: jest.fn(),
+};
+
+test('renders the component with props', () => {
+  render(<LoadKGProjects {...defaultProps} />);
+  
+  
+  expect(screen.getByText('To have edit permissions on a live paper')).toBeInTheDocument();
+});
+
+// You can write more test cases for different component behaviors
